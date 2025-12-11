d1e4182511317b436c412c626bdea301053974af
Sietse Taams <sp.taams@gmail.com>
2025-12-11 12:39:47 +0100

    chore: undo landing page changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-"use client";
-
-import { UserButton, useUser } from "@clerk/nextjs";
-
 export default function Home() {
-  const { user } = useUser();
   return (
-    <div className="flex min-h-svh items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Hi {user?.firstName || "stranger"}!
-      <UserButton />
-    </div>
+    <div className="flex min-h-svh items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
   );
 }
